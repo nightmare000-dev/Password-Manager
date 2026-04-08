d712920e60ee11d328818fc94ea54abf559a354b
nightmare000-dev <192535655+nightmare000-dev@users.noreply.github.com>
2026-04-08 10:02:50 +0300

    Add: crossplatform clear function

--- functions.py
+++ functions.py
@@ -11,18 +11,22 @@ from vocabulary import *
 fgl = Figlet(font="slant", justify="center")  # initialization Figlet
 
 
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 class Functions:
     @staticmethod
     def open_generator():  # directs to the Generator page and clears terminal
-        os.system("clear")
+        clear()
         print(fgl.renderText(TITLES[1]))
 
     @staticmethod
     def open_list():  # directs to the List page and clears terminal
-        os.system("clear")
+        clear()
         print(fgl.renderText(TITLES[3]))
 
     @staticmethod
     def open_helper():  # directs to the Helper page and clears terminal
-        os.system("clear")
+        clear()
         print(fgl.renderText(TITLES[2]))
